b9a8726d5187df02d2151408f16563cd62ff8b5c
sean lewis <seanmartinlewis@gmail.com>
2016-11-01 20:14:18 -0500

    changed names and inputed them to h2

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ class App extends Component {
           <div id="logo"><img src={logo} alt="General Assembly logo" /></div>
           <div id="title">Bank of GA</div>
         </div>
-        <Account />
-        <Account />
+        <Account name="checking"/>
+        <Account name="savings"/>
         <div className="clear"></div>
       </div>
     );
